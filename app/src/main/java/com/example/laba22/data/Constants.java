ec7f22327ffc9ef9a7064bea8f5ac98969f71a05
LabWorkDispatcher <yanshchcoll@gmail.com>
2024-02-23 00:00:45 +0300

    First commit of LabWork24-25.

---
+++ app/src/main/java/com/example/laba22/data/Constants.java
@@ -0,0 +1,8 @@
+package com.example.laba22.data;
+
+public class Constants {
+    public static String[] APP_WIDGET_ITEM_LIST = new String[]{"youtube.com", "google.com", "gmail.com"};
+    public static String APP_LINK_VARIABLE_KEY = "LINK_VAR_ID";
+    public static String APP_PREFERENCE_FILE_KEY = "BROWSER_S_PREFERENCES";
+}
+
